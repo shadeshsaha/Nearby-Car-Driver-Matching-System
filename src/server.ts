a48b7ac8fc5293fa05ec61d8f9908496e2a0061b
shadeshsaha <shadesh15-8599@diu.edu.bd>
2026-02-18 13:55:21 +0600

    Deployed on Vercel

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,6 @@ import app from "./app";
 import prisma from "./app/config/prisma";
 import config from "./config";
 
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
-
 async function nearbyCarAndDriverMatchingSystem() {
   const server: Server = app.listen(config.port, () => {
     console.info(`🚀 Server running on port ${config.port}`);
@@ -44,40 +38,3 @@ async function nearbyCarAndDriverMatchingSystem() {
 }
 
 nearbyCarAndDriverMatchingSystem();
-
-// import cors from "cors";
-// import express from "express";
-// import globalErrorHandler from "./app/middlewares/globalErrorHandler";
-// import routes from "./app/routes";
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api", routes);
-
-// app.use(globalErrorHandler);
-
-// export default app;
-
-// // import bodyParser from "body-parser";
-// // import cors from "cors";
-// // import dotenv from "dotenv";
-// // import express from "express";
-// // import rideRoutes from "./routes/ride.routes";
-
-// // dotenv.config();
-
-// // const app = express();
-// // const PORT = 3000;
-
-// // app.use(cors());
-// // app.use(bodyParser.json());
-// // // app.use(express.json());
-
-// // app.use("/api/ride", rideRoutes);
-
-// // app.listen(PORT, () => {
-// //   console.log(`Server running on http://localhost:${PORT}`);
-// // });
