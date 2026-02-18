bcfbf65bf11a08b414c4e095183124fc7ad12a74
shadeshsaha <shadesh15-8599@diu.edu.bd>
2026-02-18 11:52:32 +0600

    Centralized asyncHandler and Refactored Controller File

--- src/app/modules/ride/ride.controller.ts
+++ src/app/modules/ride/ride.controller.ts
@@ -1,19 +1,16 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
+import asyncHandler from "../../utils/asyncHandler";
 import { RideService } from "./ride.service";
 
-const requestRide = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await RideService.requestRide(req.body);
+const requestRide = asyncHandler(async (req: Request, res: Response) => {
+  const result = await RideService.requestRide(req.body);
 
-    res.status(200).json({
-      success: true,
-      message: "Nearby drivers retrieved successfully",
-      available_drivers: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: "Nearby drivers retrieved successfully",
+    available_drivers: result,
+  });
+});
 
 export const RideController = {
   requestRide,
